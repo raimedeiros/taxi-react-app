45a87669f26133c6fc8a4b5760dee784f555097f
Rai Medeiros <raismedeiros@gmail.com>
2021-12-17 00:23:10 -0300

    authentication settings

--- server/trips/views.py
+++ server/trips/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics
+from rest_framework_simplejwt.views import TokenObtainPairView 
+from .serializers import LogInSerializer, UserSerializer
 
-# Create your views here.
+
+class SignUpView(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+class LogInView(TokenObtainPairView): # new
+  serializer_class = LogInSerializer
