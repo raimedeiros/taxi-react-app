9da9600081f5a447d4f3645d4a8a443fa45dcd35
Rai Medeiros <raismedeiros@gmail.com>
2021-12-18 18:47:35 -0300

    Updating API to list trips

--- server/trips/views.py
+++ server/trips/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics
+from rest_framework import generics,permissions, viewsets
 from rest_framework_simplejwt.views import TokenObtainPairView 
-from .serializers import LogInSerializer, UserSerializer
-
+from .serializers import TripSerializer, LogInSerializer, UserSerializer
+from .models import Trip 
 
 class SignUpView(generics.CreateAPIView):
     queryset = get_user_model().objects.all()
@@ -10,3 +10,16 @@ class SignUpView(generics.CreateAPIView):
 
 class LogInView(TokenObtainPairView): # new
   serializer_class = LogInSerializer
+
+
+class TripView(viewsets.ReadOnlyModelViewSet):
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
+
+class TripView(viewsets.ReadOnlyModelViewSet):
+    lookup_field = 'id' # new
+    lookup_url_kwarg = 'trip_id' # new
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
