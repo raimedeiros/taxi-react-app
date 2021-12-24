be5351342b3f21cb9d4702334aab327f9a0f350b
Rai Medeiros <raismedeiros@gmail.com>
2021-12-23 23:38:08 -0300

    Support for roles

--- server/trips/views.py
+++ server/trips/views.py
@@ -3,6 +3,8 @@ from rest_framework import generics,permissions, viewsets
 from rest_framework_simplejwt.views import TokenObtainPairView 
 from .serializers import TripSerializer, LogInSerializer, UserSerializer
 from .models import Trip 
+from django.db.models import Q
+
 
 class SignUpView(generics.CreateAPIView):
     queryset = get_user_model().objects.all()
@@ -13,13 +15,17 @@ class LogInView(TokenObtainPairView): # new
 
 
 class TripView(viewsets.ReadOnlyModelViewSet):
+    lookup_field = 'id'
+    lookup_url_kwarg = 'trip_id'
     permission_classes = (permissions.IsAuthenticated,)
-    queryset = Trip.objects.all()
     serializer_class = TripSerializer
 
-class TripView(viewsets.ReadOnlyModelViewSet):
-    lookup_field = 'id' # new
-    lookup_url_kwarg = 'trip_id' # new
-    permission_classes = (permissions.IsAuthenticated,)
-    queryset = Trip.objects.all()
-    serializer_class = TripSerializer
+    def get_queryset(self): # new
+        user = self.request.user
+        if user.group == 'driver':
+            return Trip.objects.filter(
+                Q(status=Trip.REQUESTED) | Q(driver=user)
+            )
+        if user.group == 'rider':
+            return Trip.objects.filter(rider=user)
+        return Trip.objects.none()
