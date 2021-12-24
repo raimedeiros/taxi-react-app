877cbfbe0a09b10c37afa3b1ba33d0faad50ed82
Rai Medeiros <raismedeiros@gmail.com>
2021-12-24 00:39:03 -0300

    UI customization

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,11 @@
+import 'bootswatch/dist/lumen/bootstrap.css'; // new
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,7 +15,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
