c7276847dcd1fc4ceb69879bc2096cc6e1013894
Rai Medeiros <raismedeiros@gmail.com>
2021-12-23 23:00:02 -0300

    Introduction to websockets

--- server/taxi/routing.py
+++ server/taxi/routing.py
@@ -1,7 +1,16 @@
 from django.core.asgi import get_asgi_application
+from django.urls import path # new
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter # changed
+from taxi.middleware import TokenAuthMiddlewareStack # new
+from trips.consumers import TaxiConsumer
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
+    # new
+    'websocket': TokenAuthMiddlewareStack( # changed
+        URLRouter([
+            path('taxi/', TaxiConsumer.as_asgi()),
+        ])
+    ),
 })
---
+++ start.md
@@ -0,0 +1,6 @@
+python3.9 -m venv env
+source env/bin/activate
+
+export PGDATABASE=taxi
+export PGUSER=taxi
+export PGPASSWORD=taxi
