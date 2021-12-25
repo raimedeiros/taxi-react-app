7a0c2380a7ec86c75bb3737783d118477f589e4d
Rai Medeiros <raismedeiros@gmail.com>
2021-12-25 18:59:26 -0300

    Signup fields validation

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,106 +1,167 @@
-import React, { useState } from 'react';
-import { Formik } from 'formik'; // new
-import {
-  Breadcrumb, Button, Card, Col, Form, Row
-} from 'react-bootstrap'; // changed
-import { Link, Redirect } from 'react-router-dom';
+import React, { useState } from "react";
+import { Formik } from "formik"; // new
+import { Breadcrumb, Button, Card, Col, Form, Row } from "react-bootstrap"; // changed
+import { Link, Redirect } from "react-router-dom";
+import axios from "axios";
 
-function SignUp (props) {
+function SignUp(props) {
   const [isSubmitted, setSubmitted] = useState(false);
 
-  const onSubmit = (values, actions) => setSubmitted(true);
+  const onSubmit = async (values, actions) => {
+    const url = "/api/sign_up/";
+    const formData = new FormData();
+    formData.append("username", values.username);
+    formData.append("first_name", values.firstName);
+    formData.append("last_name", values.lastName);
+    formData.append("password1", values.password);
+    formData.append("password2", values.password);
+    formData.append("group", values.group);
+    formData.append("photo", values.photo);
+    try {
+      await axios.post(url, formData);
+      setSubmitted(true);
+    } catch (response) {
+      const data = response.response.data;
+      for (const value in data) {
+        actions.setFieldError(value, data[value].join(" "));
+      }
+    }
+  };
 
   if (isSubmitted) {
-    return <Redirect to='/log-in' />
+    return <Redirect to="/log-in" />;
   }
   return (
     <Row>
       <Col lg={12}>
         <Breadcrumb>
-          <Breadcrumb.Item href='/'>Home</Breadcrumb.Item>
+          <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
           <Breadcrumb.Item active>Sign up</Breadcrumb.Item>
         </Breadcrumb>
-        <Card className='mb-3'>
+        <Card className="mb-3">
           <Card.Header>Sign up</Card.Header>
           <Card.Body>
             <Formik
               initialValues={{
-                username: '',
-                firstName: '',
-                lastName: '',
-                password: '',
-                group: 'rider',
-                photo: []
+                username: "",
+                firstName: "",
+                lastName: "",
+                password: "",
+                group: "rider",
+                photo: [],
               }}
               onSubmit={onSubmit}
             >
               {({
+                errors,
                 handleChange,
                 handleSubmit,
-                values
+                isSubmitting,
+                setFieldValue,
+                values,
               }) => (
                 <Form noValidate onSubmit={handleSubmit}>
-                  <Form.Group controlId='username'>
+                  <Form.Group controlId="username">
                     <Form.Label>Username:</Form.Label>
                     <Form.Control
-                      name='username'
+                      className={"username" in errors ? "is-invalid" : ""}
+                      name="username"
                       onChange={handleChange}
                       values={values.username}
+                      required
                     />
+                    {"username" in errors && (
+                      <Form.Control.Feedback type="invalid">
+                        {errors.username}
+                      </Form.Control.Feedback>
+                    )}
                   </Form.Group>
-                  <Form.Group controlId='firstName'>
+                  <Form.Group controlId="firstName">
                     <Form.Label>First name:</Form.Label>
                     <Form.Control
-                      name='firstName'
+                      className={"firstName" in errors ? "is-invalid" : ""}
+                      name="firstName"
                       onChange={handleChange}
                       values={values.firstName}
                     />
+                    {"firstName" in errors && (
+                      <Form.Control.Feedback type="invalid">
+                        {errors.firstName}
+                      </Form.Control.Feedback>
+                    )}
                   </Form.Group>
-                  <Form.Group controlId='lastName'>
+                  <Form.Group controlId="lastName">
                     <Form.Label>Last name:</Form.Label>
                     <Form.Control
-                      name='lastName'
+                      className={"lastName" in errors ? "is-invalid" : ""}
+                      name="lastName"
                       onChange={handleChange}
                       values={values.lastName}
                     />
+                    {"lastName" in errors && (
+                      <Form.Control.Feedback type="invalid">
+                        {errors.lastName}
+                      </Form.Control.Feedback>
+                    )}
                   </Form.Group>
-                  <Form.Group controlId='password'>
+                  <Form.Group controlId="password">
                     <Form.Label>Password:</Form.Label>
                     <Form.Control
-                      name='password'
+                      className={"password" in errors ? "is-invalid" : ""}
+                      name="password"
                       onChange={handleChange}
-                      type='password'
+                      type="password"
                       value={values.password}
                     />
+                    {"password" in errors && (
+                      <Form.Control.Feedback type="invalid">
+                        {errors.password}
+                      </Form.Control.Feedback>
+                    )}
                   </Form.Group>
-                  <Form.Group controlId='group'>
+                  <Form.Group controlId="group">
                     <Form.Label>Group:</Form.Label>
                     <Form.Control
-                      as='select'
-                      name='group'
+                      as="select"
+                      className={"group" in errors ? "is-invalid" : ""}
+                      name="group"
                       onChange={handleChange}
                       value={values.group}
                     >
-                      <option value='rider'>Rider</option>
-                      <option value='driver'>Driver</option>
+                      <option value="rider">Rider</option>
+                      <option value="driver">Driver</option>
                     </Form.Control>
+                    {"group" in errors && (
+                      <Form.Control.Feedback type="invalid">
+                        {errors.group}
+                      </Form.Control.Feedback>
+                    )}
                   </Form.Group>
-                  <Form.Group controlId='photo'>
+                  <Form.Group controlId="photo">
                     <Form.Label>Photo:</Form.Label>
                     <Form.Control
-                      name='photo'
-                      onChange={handleChange}
-                      type='file'
-                      value={values.photo}
+                      className={"photo" in errors ? "is-invalid" : ""}
+                      name="photo"
+                      onChange={event => {
+                        setFieldValue("photo", event.currentTarget.files[0]);
+                      }}
+                      type="file"
                     />
+                    {"photo" in errors && (
+                      <Form.Control.Feedback type="invalid">
+                        {errors.photo}
+                      </Form.Control.Feedback>
+                    )}
                   </Form.Group>
-                  <Button block type='submit' variant='primary'>Sign up</Button>
+                  <Button block type="submit" variant="primary">
+                    Sign up
+                  </Button>
                 </Form>
               )}
             </Formik>
           </Card.Body>
-          <p className='mt-3 text-center'>
-            Already have an account? <Link to='/log-in'>Log in!</Link>
+          <p className="mt-3 text-center">
+            Already have an account? <Link to="/log-in">Log in!</Link>
           </p>
         </Card>
       </Col>
