8112c941aeda317a716f1252e67b26828176a43c
Rai Medeiros <raismedeiros@gmail.com>
2022-01-05 10:20:21 -0300

    add trips tests

--- client/cypress/integration/driver.spec.js
+++ client/cypress/integration/driver.spec.js
@@ -7,6 +7,57 @@ const riderEmail = faker.internet.email();
 const riderFirstName = faker.name.firstName();
 const riderLastName = faker.name.lastName();
 
+const tripResponse = [
+  {
+    id: "94fc5eba-de7a-44b2-8000-856ec824609d",
+    created: "2020-08-18T21:41:08.112946Z",
+    updated: "2020-08-18T21:41:08.112986Z",
+    pick_up_address: "A",
+    drop_off_address: "B",
+    status: "STARTED",
+    driver: {
+      id: 113,
+      photo: "http://localhost:8003/media/photos/photo_QI0TTYh.jpg",
+    },
+    rider: {
+      id: 112,
+      photo: "http://localhost:8003/media/photos/photo_r3XrvgH.jpg",
+    },
+  },
+  {
+    id: "94fc5eba-de7a-44b2-8000-856ec824609d",
+    created: "2020-08-18T21:41:08.112946Z",
+    updated: "2020-08-18T21:41:08.112986Z",
+    pick_up_address: "A",
+    drop_off_address: "B",
+    status: "REQUESTED",
+    driver: {
+      id: 113,
+      photo: "http://localhost:8003/media/photos/photo_QI0TTYh.jpg",
+    },
+    rider: {
+      id: 112,
+      photo: "http://localhost:8003/media/photos/photo_r3XrvgH.jpg",
+    },
+  },
+  {
+    id: "94fc5eba-de7a-44b2-8000-856ec824609d",
+    created: "2020-08-18T21:41:08.112946Z",
+    updated: "2020-08-18T21:41:08.112986Z",
+    pick_up_address: "A",
+    drop_off_address: "B",
+    status: "COMPLETED",
+    driver: {
+      id: 113,
+      photo: "http://localhost:8003/media/photos/photo_QI0TTYh.jpg",
+    },
+    rider: {
+      id: 112,
+      photo: "http://localhost:8003/media/photos/photo_r3XrvgH.jpg",
+    },
+  },
+];
+
 describe("The driver dashboard", function () {
   // new
   before(function () {
@@ -30,5 +81,44 @@ describe("The driver dashboard", function () {
 
     cy.visit("/#/driver");
     cy.hash().should("eq", "#/driver");
+  });
+
+  it("Displays messages for no trips", function () {
+    cy.intercept("trip", {
+      statusCode: 200,
+      body: [],
+    }).as("getTrips");
+
+    cy.logIn(riderEmail);
+
+    cy.visit("/#/rider");
+    cy.wait("@getTrips");
+
+    // Current trips.
+    cy.get("[data-cy=trip-card]").eq(0).contains("No trips.");
+
+    // Completed trips.
+    cy.get("[data-cy=trip-card]").eq(1).contains("No trips.");
+  });
+
+  it("Displays current, requested, and completed trips", function () {
+    cy.intercept("trip", {
+      statusCode: 200,
+      body: tripResponse,
+    }).as("getTrips");
+
+    cy.logIn(driverEmail);
+
+    cy.visit("/#/driver");
+    cy.wait("@getTrips");
+
+    // Current trips.
+    cy.get("[data-cy=trip-card]").eq(0).contains("STARTED");
+
+    // Requested trips.
+    cy.get("[data-cy=trip-card]").eq(1).contains("REQUESTED");
+
+    // Completed trips.
+    cy.get("[data-cy=trip-card]").eq(2).contains("COMPLETED");
   });
 });
--- client/cypress/integration/rider.spec.js
+++ client/cypress/integration/rider.spec.js
@@ -7,6 +7,41 @@ const riderEmail = faker.internet.email();
 const riderFirstName = faker.name.firstName();
 const riderLastName = faker.name.lastName();
 
+const tripResponse = [
+  {
+    id: "94fc5eba-de7a-44b2-8000-856ec824609d",
+    created: "2020-08-18T21:41:08.112946Z",
+    updated: "2020-08-18T21:41:08.112986Z",
+    pick_up_address: "A",
+    drop_off_address: "B",
+    status: "STARTED",
+    driver: {
+      id: 113,
+      photo: "http://localhost:8003/media/photos/photo_QI0TTYh.jpg",
+    },
+    rider: {
+      id: 112,
+      photo: "http://localhost:8003/media/photos/photo_r3XrvgH.jpg",
+    },
+  },
+  {
+    id: "94fc5eba-de7a-44b2-8000-856ec824609d",
+    created: "2020-08-18T21:41:08.112946Z",
+    updated: "2020-08-18T21:41:08.112986Z",
+    pick_up_address: "A",
+    drop_off_address: "B",
+    status: "COMPLETED",
+    driver: {
+      id: 113,
+      photo: "http://localhost:8003/media/photos/photo_QI0TTYh.jpg",
+    },
+    rider: {
+      id: 112,
+      photo: "http://localhost:8003/media/photos/photo_r3XrvgH.jpg",
+    },
+  },
+];
+
 describe("The rider dashboard", function () {
   // new
   before(function () {
@@ -30,5 +65,41 @@ describe("The rider dashboard", function () {
 
     cy.visit("/#/rider");
     cy.hash().should("eq", "#/rider");
+  });
+
+  it("Displays messages for no trips", function () {
+    cy.intercept("trip", {
+      statusCode: 200,
+      body: [],
+    }).as("getTrips");
+
+    cy.logIn(riderEmail);
+
+    cy.visit("/#/rider");
+    cy.wait("@getTrips");
+
+    // Current trips.
+    cy.get("[data-cy=trip-card]").eq(0).contains("No trips.");
+
+    // Completed trips.
+    cy.get("[data-cy=trip-card]").eq(1).contains("No trips.");
+  });
+
+  it("Displays current and completed trips", function () {
+    cy.intercept("trip", {
+      statusCode: 200,
+      body: tripResponse,
+    }).as("getTrips");
+
+    cy.logIn(riderEmail);
+
+    cy.visit("/#/rider");
+    cy.wait("@getTrips");
+
+    // Current trips.
+    cy.get("[data-cy=trip-card]").eq(0).contains("STARTED");
+
+    // Completed trips.
+    cy.get("[data-cy=trip-card]").eq(1).contains("COMPLETED");
   });
 });
